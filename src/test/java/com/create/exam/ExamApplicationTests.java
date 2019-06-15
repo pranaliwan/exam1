05fc227a1ced7503a6fcc32c2187c2e8069dc27c
group007 <pranaliWankhade94@gmail.com>
2019-06-15 11:45:44 +0530

    second commit

--- src/test/java/com/create/exam/ExamApplicationTests.java
+++ src/test/java/com/create/exam/ExamApplicationTests.java
@@ -1,12 +1,7 @@
 package com.create.exam;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.testng.annotations.Test;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class ExamApplicationTests {
 
 	@Test
